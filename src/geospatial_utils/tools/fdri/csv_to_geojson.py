833671b03a6d5138135508dfe65d71a6e5225e3d
amber barr <ambbar@ceh.ac.uk>
2026-03-16 15:47:05 +0000

    changes to file

---
+++ src/geospatial_utils/tools/fdri/csv_to_geojson.py
@@ -0,0 +1,144 @@
+"""Script to convert csv metadata into geojson format"""
+
+import argparse
+import logging
+import os
+from pathlib import Path
+from types import SimpleNamespace
+
+import geojson
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+COMMAND = "obs_fdri_to_geojson"
+DESCRIPTION = "Convert gauging csv files into geojson to store metadata."
+
+
+def add_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    parser.add_argument(
+        "--output_geojson_dir",
+        required=True,
+        type=Path,
+        help=("directory to store all geoson files containing metadata."),
+    )
+
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument("--input_csv_path", type=Path, help="Path to the csv to be converted")
+    input_group.add_argument(
+        "--input_csv_dir",
+        type=Path,
+        help=(
+            "Path to the directory containing csvs to be converted. All .csv files found within this directory "
+            "will be processed."
+        ),
+    )
+
+    return parser
+
+
+def main() -> None:
+    """Entrypoint to the script."""
+
+    parser = argparse.ArgumentParser(prog=COMMAND, description=DESCRIPTION)
+
+    parser = add_arguments(parser)
+    args = parser.parse_args()
+
+    run_from_cli(args)
+
+
+def run_from_cli(args: SimpleNamespace) -> None:
+    """The entrypoint when running from the centralised CLI."""
+    # Call the main run function
+    run(
+        input_csv_path=args.input_csv_path,
+        input_csv_dir=args.input_csv_dir,
+        output_geojson_dir=args.output_geojson_dir,
+    )
+
+
+def run(output_geojson_dir: str | Path, input_csv_path: str | Path = None, input_csv_dir: str | Path = None) -> None:
+    # create the output directory if it doesn't already exist
+    os.makedirs(output_geojson_dir, exist_ok=True)
+
+    if input_csv_path is not None:
+        csv_to_geojson(input_csv_path=input_csv_path, output_geojson_dir=output_geojson_dir)
+
+    else:
+        input_csv_dir = Path(input_csv_dir)
+
+        for input_csv_path in input_csv_dir.glob("*.csv"):
+            logger.info(f"Converting {input_csv_path.name} to geojson")
+
+            csv_to_geojson(input_csv_path=input_csv_path, output_geojson_dir=output_geojson_dir)
+
+    print("finished")
+
+
+def csv_to_geojson(input_csv_path: str | Path, output_geojson_dir: str | Path) -> None:
+    """takes the metadata in csv format and creates a geojson file
+
+    inputs: a directory with multiple csvs to process or an individual metadata.csv
+
+    outputs: metadata.geojson
+
+    """
+    # open the csv file
+    df = pd.read_csv(input_csv_path, encoding="utf-8-sig")
+
+    # normalise columns first
+    df.columns = df.columns.str.strip()
+
+    # create a mapping to create consistant column names in all fdri observatory csv files.
+    possible_name_key = {
+        "Start_date": ["start_Date", "Data Start", "data_start", "Data_start", "start_date", "oldest_survey"],
+        "End_date": ["Data End", "Data_end", "newest_survey"],
+        "Site_ID": ["AWS_ID", "Site ID", "Station_number", "SiteID"],
+        "Altitude": ["altitude", "Altitude_m"],
+        "Name": ["AWS_name", "Site Name", "SiteName", "AWS_Name"],
+        "Latitude": ["lat", "latitude"],
+        "Longitude": ["lon", "longitude"],
+    }
+
+    # create an empty dictionary to store standardised column headers.
+    resolved_columns = {}
+
+    # check for each alias name and store the corresponding standard name.
+    for standard_name, aliases in possible_name_key.items():
+        for alias in aliases:
+            if alias in df.columns:
+                resolved_columns[alias] = standard_name
+                break
+
+    # rename the column headers to the newly matched ones.
+    df = df.rename(columns=resolved_columns)
+
+    # store features to turn into geojson
+    metadata_features = []
+
+    # iterate through all the rows (ignoring index) in the csv and store the Point geometry from lat and lon.
+    for _, row in df.iterrows():
+        lon, lat = (float(row["Longitude"]), float(row["Latitude"]))
+        geometry = geojson.Point((lon, lat))
+
+        # prepare properties
+        meta_properties = {col: row[col] for col in possible_name_key if col in df.columns and pd.notnull(row[col])}
+
+        # create geojson features
+        feature = geojson.Feature(geometry=geometry, properties=meta_properties)
+        metadata_features.append(feature)
+
+    # output path
+    output_geojson_path = output_geojson_dir / f"{input_csv_path.stem}.geojson"
+
+    # write out to geojson
+    with open(output_geojson_path, "w", encoding="utf-8") as fout:
+        feature_collection = geojson.FeatureCollection(metadata_features)
+        geojson.dump(feature_collection, fout)
+
+    print("finished")
+
+
+if __name__ == "__main__":
+    main()
